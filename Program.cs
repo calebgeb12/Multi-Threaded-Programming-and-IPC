0eddb64021b657581fd208673d88da871aeaded8
calebgeb12 <calebgeb55@gmail.com>
2025-02-27 21:12:01 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Threading;
+
+namespace MyFirstProgram;
+
+class Program {
+    static void Main(string[] args) {
+        Thread mainThread = Thread.currentThread;
+        mainThread.Name = "Main Thread";
+        Console.WriteLine(mainThread.Name + " is complete")
+
+        CountDown(10);
+        CountUp(10);
+
+    }
+
+    public static void CountDown(int n) {
+        for (int i = n; i >= 0; i++) {
+            Console.WriteLine("timer 1: " + i);
+        }
+    }
+
+    public static void CountDown(int n) {
+        for (int i = 0; i < n; i++) {
+            Console.WriteLine("timer 2: " + i);
+        }
+    }
+}
---
+++ TestProgram.cs
@@ -0,0 +1,9 @@
+using System;
+
+class TestProgram {
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        
+    }
+}
